b1789050af91f7895b2007eb65e036312664b15a
TheAmgadX <theamgadx@gmail.com>
2025-10-12 13:17:12 +0300

    feat(scripts): add script for reading CSV data into models

--- scripts/read_data_from_csv.py
+++ scripts/read_data_from_csv.py
@@ -11,7 +11,7 @@ def load_data():
     with open("../Courses.csv") as courses_file:
         csv_reader = csv.reader(courses_file, delimiter=',')
         line_count = 0
-        for row in courses_file:
+        for row in csv_reader:
             if line_count == 0:
                 line_count += 1
                 continue
@@ -21,13 +21,13 @@ def load_data():
             line_count += 1
 
 
-        print(f"number of courses: {line_count}")
+        print(f"number of courses: {line_count - 1}")
     
     ''' read instructors file data '''
     with open("../Instructors.csv") as instructors_file:
         csv_reader = csv.reader(instructors_file, delimiter=',')
         line_count = 0
-        for row in instructors_file:
+        for row in csv_reader:
             # skip first line.
             if line_count == 0:
                 line_count += 1
@@ -39,13 +39,13 @@ def load_data():
             line_count += 1
 
 
-        print(f"number of instructors: {line_count}")
+        print(f"number of instructors: {line_count - 1}")
 
     ''' read rooms file data '''
     with open("../Rooms.csv") as rooms_file:
         csv_reader = csv.reader(rooms_file, delimiter=',')
         line_count = 0
-        for row in rooms_file:
+        for row in csv_reader:
             # skip first line.
             if line_count == 0:
                 line_count += 1
@@ -56,5 +56,5 @@ def load_data():
             line_count += 1
 
 
-        print(f"number of rooms: {line_count}")
+        print(f"number of rooms: {line_count - 1}")
 
